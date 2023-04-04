18ee879eeafab90bc6e965caff325f45e788cf9c
Owen_013 <owen@crawfordsix.com>
2023-04-04 19:58:23 -0400

    v0.1.0: first version. Known issue: the animation speed may not be updated immediately if the player is bigger than 100% width and shrinks back down to normal size or smallerAdd project files.

---
+++ Owen013.HikersModSHAddon/HikersModSHAddonController.cs
@@ -0,0 +1,43 @@
+﻿using OWML.ModHelper;
+using HikersMod;
+using HarmonyLib;
+using UnityEngine;
+using SmolHatchling;
+
+namespace HikersModSHCompatibility
+{
+    public class HikersModSHAddonController : ModBehaviour
+    {
+        public void Start()
+        {
+            Harmony.CreateAndPatchAll(typeof(HikersModSHAddonController));
+            ModHelper.Console.WriteLine("Hiker's Mod compatibility addon for Smol Hatchling is ready to go!", OWML.Common.MessageType.Success);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(HikersModController), nameof(HikersModController.UpdateAnimSpeed))]
+        public static bool UpdateAnimSpeed(HikersModController __instance)
+        {
+            float multiplier = __instance._characterController._acceleration / __instance._groundAccel * SmolHatchlingController.Instance._animSpeed;
+            if (__instance._moveSpeed == MoveSpeed.Walking) __instance._animController._animator.speed = Mathf.Max(__instance._walkSpeed / 6 * multiplier, multiplier);
+            else if (__instance._moveSpeed == MoveSpeed.DreamLantern) __instance._animController._animator.speed = Mathf.Max(__instance._dreamLanternSpeed / 6 * multiplier, multiplier);
+            else __instance._animController._animator.speed = Mathf.Max(__instance._characterController._runSpeed / 6 * multiplier, multiplier);
+            return false;
+        }
+
+        //[HarmonyPrefix]
+        //[HarmonyPatch(typeof(HikersModController), nameof(HikersModController.ChangeAttributes))]
+        //public static void SetStoryModeAttributes(HikersModController __instance)
+        //{
+        //    if (StoryController.Instance._storyEnabledNow)
+        //    {
+        //        {
+        //            __instance._normalSpeed = 4;
+        //            __instance._walkSpeed = 2;
+        //            __instance._jumpPower = 5;
+        //            __instance._sprintSpeed = 8;
+        //        }
+        //    };
+        //}
+    }
+}
